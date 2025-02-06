230d256e93216bdb69f0e82de78da56ce6678fa5
Joss-Y8 <romero080505@gmail.com>
2025-02-05 18:43:14 -0600

    Se modificó función generacion_aleatoria

--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -10,4 +10,24 @@
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
     }
+
+    function generacion_repetitiva(){
+        $mtz=[]; 
+        $iteracion = 0; 
+        $numeros = 0; 
+
+        do{
+            $iteracion++; 
+            $fila = [rand(100,999), rand(100,999), rand(100,999)]; 
+            $numeros +=3; 
+            $mtz[]=$fila; 
+        }while (!($fila[0]%2 !=0 && $fila[1]%2==0 && $fila[2]%2!=0)); 
+
+        foreach($mtz as $fila){
+            echo implode(", ", $fila).'<br>'; 
+        }
+        echo '<br>'; 
+
+        echo "\n$numeros números obtenidos en $iteracion iteraciones";
+    } 
 ?>
