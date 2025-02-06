b1b2c42db71d9bbd367335e332312f337de69ae7
Joss-Y8 <romero080505@gmail.com>
2025-02-05 18:42:34 -0600

    función generacion_aleatoria llamada con exito

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -16,6 +16,15 @@
         }
     ?>
 
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+        secuencia compuesta por: impar, par, impar</p>
+    <?php
+        require_once __DIR__ .'/src/funciones.php';
+        echo '<p>Los numeros generados son: <p>';
+        generacion_repetitiva(); 
+    ?>
+
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
         Name: <input type="text" name="name"><br>
