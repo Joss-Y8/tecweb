5e86831b4f8ac62e8c789d85136365e076df5c06
Joss-Y8 <romero080505@gmail.com>
2025-03-03 21:57:12 -0600

    se modifico query para busqueda

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -1,9 +1,9 @@
-<?php
+ <?php
     include_once __DIR__.'/database.php';
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
+    /* SE VERIFICA HABER RECIBIDO EL ID
     if( isset($_POST['id']) ) {
         $id = $_POST['id'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
@@ -18,7 +18,18 @@
                 }
             }
 			$result->free();
-		} else {
+		} */
+    // SE VERIFICA HABER RECIBIDO EL ID O PALABRA CLAVE
+    if(isset($_POST['id'])) {
+        $id = $conexion->real_escape_string($_POST['id']); // Evitamos inyecciones SQL
+        $query = "SELECT * FROM productos WHERE id = '$id' OR nombre LIKE '%$id%' OR marca LIKE '%$id%' OR modelo LIKE '%$id%' OR detalles LIKE '%$id%'";
+        
+        if($result = $conexion->query($query)) {
+            while($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            $result->free();
+            } else {
             die('Query Error: '.mysqli_error($conexion));
         }
 		$conexion->close();
