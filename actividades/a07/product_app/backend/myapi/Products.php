2be419e45fc5c3a5827a07cd54e081f3b7d11542
Joss-Y8 <romero080505@gmail.com>
2025-03-21 19:04:12 -0600

    se agregó lógica a la función list()

--- actividades/a07/product_app/backend/myapi/Products.php
+++ actividades/a07/product_app/backend/myapi/Products.php
@@ -21,7 +21,27 @@
         }
 
         public function list(){
+            //CREAMOS EL ARREGLO A DEVOLVERSE EN FORMA JSON
+            $this->data=array(); 
 
+            //Query de búsqueda y validación de resultados 
+            if($result = $this->conexion->query("SELECT * FROM productos WHERE eliminado = 0")){
+
+                //obtenemos resultados
+                $rows = $result->fetch_all(MYSQLI_ASSOC); 
+                if(!is_null($rows)){
+                    //codificación a UTF-8 de datos y mapeo al arreglo de respuesta
+                    foreach($rows as $num => $row){
+                        foreach($row as $key => $value){
+                            $this ->data[$num][$key]=utf8_encode($value); 
+                        }
+                    }
+                }
+                $result->free(); 
+            } else{
+                die('Query Error: '.mysqli_error($this->conexion)); 
+            }
+            $this->conexion->close(); 
         }
 
         public function search($ser){
@@ -37,7 +57,7 @@
         }
 
         public function getData(){
-
+            return json_encode($this->data, JSON_PRETTY_PRINT); 
         }
     }
 ?>
