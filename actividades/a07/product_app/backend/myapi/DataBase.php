f7d8ad4de1e88bb64a4a724f66a09e77011725f9
Joss-Y8 <romero080505@gmail.com>
2025-03-23 20:42:00 -0600

    modificación para conexión a BD

--- actividades/a07/product_app/backend/myapi/DataBase.php
+++ actividades/a07/product_app/backend/myapi/DataBase.php
@@ -4,13 +4,12 @@
     abstract class DataBase{
         protected $conexion; 
 
-        public function  __construct($db, $user, $pass){
-        $this->conexion = @mysqli_connect('localhost', $db, $user, $pass);
+        public function  __construct( $user, $pass, $db){
+        $this->conexion = @mysqli_connect('localhost',$user, $pass, $db);
 
-            if(!$this->conexion) {
+            if(!$this->conexion){ 
                 die('¡Base de datos NO conectada!');
             }
         }
-    
     }
 ?>
