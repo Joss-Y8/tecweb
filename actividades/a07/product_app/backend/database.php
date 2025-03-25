afb07c72d256fb444d2b9eb161bfe2769a84b19d
Joss-Y8 <romero080505@gmail.com>
2025-03-24 18:14:20 -0600

    se creo la instancia para conectar a BD

--- actividades/a07/product_app/backend/database.php
+++ actividades/a07/product_app/backend/database.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = @mysqli_connect(
+    /*$conexion = @mysqli_connect(
         'localhost',
         'root',
         'jojoyrl8',
@@ -9,7 +9,13 @@
     /**
      * NOTA: si la conexión falló $conexion contendrá false
      **/
-    if(!$conexion) {
+    /*if(!$conexion) {
         die('¡Base de datos NO conextada!');
-    }
+    }*/
+
+    use TECWEB\MYAPI\Products as Products; 
+    require_once __DIR__.'./myapi/Products.php'; 
+    $prodObj = new Products ('marketzone');
+    echo json_encode($prodObj->getData());
+
 ?>
