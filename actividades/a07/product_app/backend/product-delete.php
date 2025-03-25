67160179cd0c42e4d147122460a16752e1a1d000
Joss-Y8 <romero080505@gmail.com>
2025-03-24 18:18:54 -0600

    se agregó uso de la función delete

--- actividades/a07/product_app/backend/product-delete.php
+++ actividades/a07/product_app/backend/product-delete.php
@@ -1,5 +1,5 @@
 <?php
-    include_once __DIR__.'/database.php';
+    /*include_once __DIR__.'/database.php';
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array(
@@ -21,5 +21,14 @@
     } 
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    echo json_encode($data, JSON_PRETTY_PRINT);*/
+
+    use TECWEB\MYAPI\Products as Products; 
+    require_once __DIR__.'/myapi/Products.php'; 
+    
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+    $prodObj = new Products('marketzone'); 
+    $prodObj->delete($id);
+    
+    echo json_encode ($prodObj->getData()); 
 ?>
