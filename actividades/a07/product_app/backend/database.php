eedeed5a7b774e367af184893f22cdb1116e22fd
Joss-Y8 <romero080505@gmail.com>
2025-03-24 18:36:21 -0600

    se eliminó código php anterior

--- actividades/a07/product_app/backend/database.php
+++ actividades/a07/product_app/backend/database.php
@@ -1,18 +1,4 @@
 <?php
-    /*$conexion = @mysqli_connect(
-        'localhost',
-        'root',
-        'jojoyrl8',
-        'marketzone'
-    );
-
-    /**
-     * NOTA: si la conexión falló $conexion contendrá false
-     **/
-    /*if(!$conexion) {
-        die('¡Base de datos NO conextada!');
-    }*/
-
     use TECWEB\MYAPI\Products as Products; 
     require_once __DIR__.'./myapi/Products.php'; 
     $prodObj = new Products ('marketzone');
