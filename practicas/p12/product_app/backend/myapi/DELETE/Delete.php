5fb743703000bb9253b5580e4b46ac35807b6720
Joss-Y8 <romero080505@gmail.com>
2025-04-02 16:04:30 -0600

    carga de carpeta product_app

---
+++ practicas/p12/product_app/backend/myapi/DELETE/Delete.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Delete extends DataBase{
+        public function __construct($db, $user='root', $pass='jojoyrl8'){ //quitar usua
+            $this->data = array(); 
+            parent::__construct($user, $pass, $db); 
+        }
+
+        public function delete ($id){
+            // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+           $this->data = array(
+               'status'  => 'error',
+               'message' => 'La consulta falló'
+           );
+           // SE VERIFICA HABER RECIBIDO EL ID
+           if( isset($_GET['id']) ) {
+               $id = $_GET['id'];
+               // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+               $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+               if ( $this->conexion->query($sql) ) {
+                   $this->data['status'] =  "success";
+                   $this->data['message'] =  "Producto eliminado";
+               } else {
+                   $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($this->conexion);
+               }
+               $this->conexion->close();
+           } 
+       }
+    }
+?>
