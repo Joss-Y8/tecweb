49b2cc89ac4b33aef432d89d62d989110d0e3c1f
Joss-Y8 <romero080505@gmail.com>
2025-04-09 19:18:03 -0600

    Carpeta de practica Slim

---
+++ practicas/p17/index.php
@@ -0,0 +1,18 @@
+<?php
+    use Psr\Http\Message\ResponseInterface as Response; 
+    use Psr\Http\Message\ServerRequestInterface as Request; 
+    use Slim\Factory\AppFactory; 
+
+    require 'vendor/autoload.php'; 
+
+    $app = AppFactory::create(); 
+    $app->setBasepath("/tecweb/practicas/p17"); 
+
+    $app->get('/', function($request, $response, $args) {
+
+        $response->getBody()->write("Hola Mundo Slim!!!");
+        return $response;  
+    });
+
+    $app->run(); 
+?>
