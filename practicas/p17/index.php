9ba3921e837d01fd31a71e9217382687c088ff8d
Joss-Y8 <romero080505@gmail.com>
2025-04-09 20:43:32 -0600

    creación de testjason con metodo post

--- practicas/p17/index.php
+++ practicas/p17/index.php
@@ -28,5 +28,32 @@
         $response->getBody()->write("Valores: ". $val1." ".$val2);
         return $response;  
     }); 
+
+    #testjson con get
+    $app->get("/testjson1", function($request, $response, $args){
+        $data[0]["nombre"]="Valeria";
+        $data[0]["apellidos"]="Pestaña Marquez";
+        $data[1]["nombre"]="Mafer";
+        $data[1]["apellidos"]="Ruiz Santiago";
+        $data[2]["nombre"]="Jossette";
+        $data[2]["apellidos"]="Romero Lagunes";
+
+        $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+        return $response; 
+    }); 
+
+    #testjson con post
+    $app->post("/testjson", function($request, $response, $args){
+        $reqPost = $request->getParsedBody(); 
+        $data[0]["nombre"]= $reqPost["nombre1"];
+        $data[0]["apellidos"]= $reqPost["apellidos1"];
+        $data[1]["nombre"]= $reqPost["nombre2"];
+        $data[1]["apellidos"]= $reqPost["apellidos2"];
+
+        $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+        return $response; 
+    });
+
+
     $app->run(); 
 ?>
