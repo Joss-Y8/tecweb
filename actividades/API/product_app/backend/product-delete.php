9d76fa3838e18d90899cf78240e3147fd2419bea
Joss-Y8 <romero080505@gmail.com>
2025-04-28 20:50:57 -0600

    Carpeta de actividada API Rest

---
+++ actividades/API/product_app/backend/product-delete.php
@@ -0,0 +1,11 @@
+<?php
+     require_once __DIR__ . '/vendor/autoload.php'; 
+
+     use TECWEB\MYAPI\DELETE\Delete as Delete; 
+    
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+    $prodObj = new Delete('marketzone'); 
+    $prodObj->delete($id);
+    
+    echo json_encode ($prodObj->getData()); 
+?>
