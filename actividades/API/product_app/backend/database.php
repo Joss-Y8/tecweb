29b1cb197ff4ac2f73887771dfcf1c7d593ab2ad
Joss-Y8 <romero080505@gmail.com>
2025-04-29 17:11:50 -0600

    se eliminaron archivos product no necesarios

--- actividades/API/product_app/backend/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once __DIR__ . '/vendor/autoload.php'; 
-    
-    use TECWEB\MYAPI\READ\Read as Read; 
-
-    $prodObj = new Read('marketzone');
-    echo json_encode($prodObj->getData());
-
-?>
